3ae5b4214377495246a7e7a0fe12c3e859933d56
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-27 17:41:12 +0530

    provider added

--- Smart_Home_Management/smart_home_app/lib/main.dart
+++ Smart_Home_Management/smart_home_app/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_home_app/controller/room_controller.dart';
+import 'package:smart_home_app/view/HomeScreen/home_screen.dart';
+import 'package:smart_home_app/view/RoomDetails/room_details_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +13,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context)=>RoomController(), //Provider
+      child:  MaterialApp(
+        home:const HomeScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          "roomdetails":(context)=>const RoomDetailsScreen(),
+        },
       ),
     );
   }
