6b90aa87f0f672dd9ab38e669805c580d24e1721
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-02 18:39:01 +0530

    splash screen

---
+++ Wallet_App/wallet_app/lib/view/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+///WIDGET TO DISPLAY SPLASHSCREEN
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 1200),()=>Get.off("/login"));
+    return Scaffold(
+      body: Image.asset(
+        "assets/images/splash.png",height: Get.height,width: Get.width,fit: BoxFit.cover,
+      ),
+    );
+}
+}
