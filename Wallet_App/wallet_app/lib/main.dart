e1119caff86c054a0a633f59796a9ce29bb6bc64
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-04 19:03:54 +0530

    updated

--- Wallet_App/wallet_app/lib/main.dart
+++ Wallet_App/wallet_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wallet_app/controller/controller_binding.dart';
+import 'package:wallet_app/view/HistoryScreen/history_screen.dart';
+import 'package:wallet_app/view/HomeScreen/home_screen.dart';
 import 'package:wallet_app/view/LoginScreen/create_account_screen.dart';
 import 'package:wallet_app/view/LoginScreen/login_screen.dart';
 import 'package:wallet_app/view/LoginScreen/otp_screen.dart';
@@ -17,12 +20,17 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       initialRoute: "/",
+      initialBinding:ControllerBinding() ,
       getPages: [
         GetPage(name: "/", page: () => const SplashScreen()),
         GetPage(name: "/login", page: () => const LoginScreen()),
         GetPage(name: "/password", page: () => const PasswordScreen()),
         GetPage(name: "/createAc", page: () => const CreateAccountScreen()),
         GetPage(name: "/otpScreen", page: () => const OTPScreen()),
+        GetPage(name: "/home", page: () => const HomeScreen()),
+        GetPage(name: "/history", page: () => const HistoryScreen()),
+
+
       ],
       debugShowCheckedModeBanner: false,
     );
