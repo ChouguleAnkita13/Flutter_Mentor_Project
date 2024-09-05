24fc8945ca3c890d5e1105a5b14aa11d283ac039
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-05 15:58:54 +0530

    controller added

---
+++ Wallet_App/wallet_app/lib/controller/more_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:wallet_app/model/more_model.dart';
+
+class MoreController extends GetxController {
+  final List<MoreModel> moreItemList = [
+    const MoreModel(
+        moreImgUrl: "assets/images/more/paybill.png", moreTitle: "Pay bills"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/transfer.png", moreTitle: "Transfer"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/topup.png", moreTitle: "Topup"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/withdraw.png", moreTitle: "Withdraw"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/analytics.png", moreTitle: "Analytics"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/help.png", moreTitle: "Help"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/contactus.png",
+        moreTitle: "Contact us"),
+    const MoreModel(
+        moreImgUrl: "assets/images/more/about.png", moreTitle: "About"),
+  ];
+}
