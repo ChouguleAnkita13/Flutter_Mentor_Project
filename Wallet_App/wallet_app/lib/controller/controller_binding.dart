2e6c1cbea11097815e6490af0e25bb6137d429eb
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-05 16:04:37 +0530

    bind morecontroller

--- Wallet_App/wallet_app/lib/controller/controller_binding.dart
+++ Wallet_App/wallet_app/lib/controller/controller_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'package:wallet_app/controller/more_controller.dart';
 import 'package:wallet_app/controller/transaction_controller.dart';
 
 class ControllerBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put(TransactionController ()); 
+    Get.put(TransactionController());
+    Get.put(MoreController());
   }
 }
