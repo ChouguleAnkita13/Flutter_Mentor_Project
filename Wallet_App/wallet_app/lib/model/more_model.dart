a64d9d0951c18560e82445a7e9bbca9c708e9636
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-05 15:57:01 +0530

    more model created

---
+++ Wallet_App/wallet_app/lib/model/more_model.dart
@@ -0,0 +1,7 @@
+///MODEL CLASS FOR MORESCREEN ITEMS
+class MoreModel{
+  final String moreImgUrl; ///IMAGE URL FOR ITEM
+  final String moreTitle; ///TITLE OF ITEM
+
+  const MoreModel({required this.moreImgUrl,required this.moreTitle});
+}
