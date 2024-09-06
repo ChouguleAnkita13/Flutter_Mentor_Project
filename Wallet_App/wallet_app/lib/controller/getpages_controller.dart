29c7d33fb4c1aaa89b20d7ec2690594c6f07da5e
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-06 17:17:08 +0530

    profile screen route

--- Wallet_App/wallet_app/lib/controller/getpages_controller.dart
+++ Wallet_App/wallet_app/lib/controller/getpages_controller.dart
@@ -5,21 +5,22 @@ import 'package:wallet_app/view/LoginScreen/create_account_screen.dart';
 import 'package:wallet_app/view/LoginScreen/login_screen.dart';
 import 'package:wallet_app/view/LoginScreen/otp_screen.dart';
 import 'package:wallet_app/view/LoginScreen/password_screen.dart';
+import 'package:wallet_app/view/ProfileScreen/profile_screen.dart';
 import 'package:wallet_app/view/aboutus_screen.dart';
 import 'package:wallet_app/view/more_screen.dart';
 import 'package:wallet_app/view/splash_screen.dart';
 
-class GetpagesController{
-  static List<GetPage<dynamic>> getPagesList=[
-        GetPage(name: "/", page: () => const SplashScreen()),
-        GetPage(name: "/login", page: () => const LoginScreen()),
-        GetPage(name: "/password", page: () => const PasswordScreen()),
-        GetPage(name: "/createAc", page: () => const CreateAccountScreen()),
-        GetPage(name: "/otpScreen", page: () => const OTPScreen()),
-        GetPage(name: "/home", page: () => const HomeScreen()),
-        GetPage(name: "/history", page: () => const HistoryScreen()),
-        GetPage(name: "/more", page: () => const MoreScreen()),
-        GetPage(name: "/about", page: () => const AboutUsScreen()),
-
-      ];
+class GetpagesController {
+  static List<GetPage<dynamic>> getPagesList = [
+    GetPage(name: "/", page: () => const SplashScreen()),
+    GetPage(name: "/login", page: () => const LoginScreen()),
+    GetPage(name: "/password", page: () => const PasswordScreen()),
+    GetPage(name: "/createAc", page: () => const CreateAccountScreen()),
+    GetPage(name: "/otpScreen", page: () => const OTPScreen()),
+    GetPage(name: "/home", page: () => const HomeScreen()),
+    GetPage(name: "/history", page: () => const HistoryScreen()),
+    GetPage(name: "/more", page: () => const MoreScreen()),
+    GetPage(name: "/about", page: () => const AboutUsScreen()),
+    GetPage(name: "/profile", page: () => const ProfileScreen())
+  ];
 }
