de6cdb9532c07f2da8883e9d79296c103bdf147f
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-10 11:41:31 +0530

    minor change

--- Wallet_App/wallet_app/lib/view/PaybillsScreen/paybills_screen.dart
+++ Wallet_App/wallet_app/lib/view/PaybillsScreen/paybills_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wallet_app/view/PaybillsScreen/Widgets/payto.dart';
@@ -15,8 +13,6 @@ class PaybillsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final deviceWidth = Get.width;
     final deviceHeight = Get.height;
-    log("${deviceWidth * 0.04}");
-    log("${deviceHeight * 0.14}");
     return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
       body: Padding(
@@ -28,23 +24,25 @@ class PaybillsScreen extends StatelessWidget {
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             ///CUSTOM BACK BUTTON
-
             const CustomBackButton(),
 
             ///
             SizedBox(height: deviceHeight * 0.04), //36
-            ///
+
+            ///PAY TO NEW BILLER
             const PayTo(),
+
+            ///
             SizedBox(height: deviceHeight * 0.02), //18
 
             ///SEARCH TEXTFIELD
             const SearchTextfield(),
+
             ///
-            SizedBox(height: deviceHeight * 0.025),//22
+            SizedBox(height: deviceHeight * 0.025), //22
+            
             ///SHOW SAVED BILLERS
             const Expanded(child: SavedBillers())
-
-            
           ])),
     );
   }
