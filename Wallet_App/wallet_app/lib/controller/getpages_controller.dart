277d7ab6e6c03ad018af07be95cd82e725c0d710
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-12 00:49:34 +0530

    routing

--- Wallet_App/wallet_app/lib/controller/getpages_controller.dart
+++ Wallet_App/wallet_app/lib/controller/getpages_controller.dart
@@ -1,8 +1,5 @@
 import 'package:get/get.dart';
 import 'package:wallet_app/view/CardScreen/card_pay_screen.dart';
-import 'package:wallet_app/view/CardScreen/card_screen.dart';
-import 'package:wallet_app/view/HistoryScreen/history_screen.dart';
-import 'package:wallet_app/view/HomeScreen/home_screen.dart';
 import 'package:wallet_app/view/LoginScreen/create_account_screen.dart';
 import 'package:wallet_app/view/LoginScreen/login_screen.dart';
 import 'package:wallet_app/view/LoginScreen/otp_screen.dart';
@@ -14,7 +11,7 @@ import 'package:wallet_app/view/TransferScreen/amount_screen.dart';
 import 'package:wallet_app/view/TransferScreen/payfailed_screen.dart';
 import 'package:wallet_app/view/TransferScreen/transfer_screen.dart';
 import 'package:wallet_app/view/aboutus_screen.dart';
-import 'package:wallet_app/view/more_screen.dart';
+import 'package:wallet_app/view/bottom_navigation_screen.dart';
 import 'package:wallet_app/view/splash_screen.dart';
 
 class GetpagesController {
@@ -24,9 +21,7 @@ class GetpagesController {
     GetPage(name: "/password", page: () => const PasswordScreen()),
     GetPage(name: "/createAc", page: () => const CreateAccountScreen()),
     GetPage(name: "/otpScreen", page: () => const OTPScreen()),
-    GetPage(name: "/home", page: () => const HomeScreen()),
-    GetPage(name: "/history", page: () => const HistoryScreen()),
-    GetPage(name: "/more", page: () => const MoreScreen()),
+    GetPage(name: "/bNavigation", page: () => const BottomNavigationScreen()),
     GetPage(name: "/about", page: () => const AboutUsScreen()),
     GetPage(name: "/profile", page: () => const ProfileScreen()),
     GetPage(name: "/paybills", page: () => const PaybillsScreen()),
@@ -34,7 +29,6 @@ class GetpagesController {
     GetPage(name: "/transfer", page: () => const TransferScreen()),
     GetPage(name: "/amountScreen", page: () => const AmountScreen()),
     GetPage(name: "/payfailed", page: () => const PayFailedScreen()),
-    GetPage(name: "/cardScreen", page: () => const CardScreen()),
     GetPage(name: "/cardPayScreen", page: () => const CardPayScreen()),
   ];
 }
