5e7c3c1bc9f09d94562b474b16d351c44922adc9
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-12 13:06:31 +0530

    comments added

--- Wallet_App/wallet_app/lib/controller/controller_binding.dart
+++ Wallet_App/wallet_app/lib/controller/controller_binding.dart
@@ -5,13 +5,23 @@ import 'package:wallet_app/controller/navigation_controller.dart';
 import 'package:wallet_app/controller/transaction_controller.dart';
 import 'package:wallet_app/controller/transferto_contact_controller.dart';
 
+/// CLASS THAT IMPLEMENTS THE BINDINGS FOR DEPENDENCY INJECTION
 class ControllerBinding implements Bindings {
   @override
   void dependencies() {
+    /// INSTANTIATING NAVIGATIONCONTROLLER FOR BOTTOM NAVIGATION MANAGEMENT
     Get.put(NavigationController());
+
+    /// INSTANTIATING TRANSFERTOCONTACTCONTROLLER FOR CONTACT TRANSFER FUNCTIONALITY
     Get.put(TransferToContactController());
+
+    /// INSTANTIATING TRANSACTIONCONTROLLER TO HANDLE TRANSACTIONS
     Get.put(TransactionController());
+
+    /// INSTANTIATING MORECONTROLLER FOR 'MORE' SCREEN FUNCTIONALITIES
     Get.put(MoreController());
+
+    /// INSTANTIATING BILLCONTROLLER TO HANDLE BILL MANAGEMENT
     Get.put(BillController());
   }
 }
