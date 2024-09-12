f0e642338325744deac09637512d64a6728b73eb
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-12 13:04:41 +0530

    model for country code

---
+++ Wallet_App/wallet_app/lib/model/countrycode_model.dart
@@ -0,0 +1,9 @@
+///MODEL FOR COUNTRY CODE
+class CountryCodeModel{
+  final String flag;///FLAG IMAGE OF COUNTRY
+  final String country;///COUNTRY NAME
+  final String code;///COUNTRY CODE
+
+  const CountryCodeModel({required this.code,required this.flag,required this.country});
+
+}
