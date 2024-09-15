8a68577677e10ecba682003e0415c86eab593742
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-15 22:46:19 +0530

    comments added

--- Wallet_App/wallet_app/lib/view/splash_screen.dart
+++ Wallet_App/wallet_app/lib/view/splash_screen.dart
@@ -1,19 +1,28 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 ///WIDGET TO DISPLAY SPLASHSCREEN
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(milliseconds: 1200),()=>Get.offAndToNamed("/login"));
-    log("hii");
+    
+    ///REDIRECT TO LOGIN SCREEN AFTER 1200 MILLISECONDS
+    Future.delayed(
+      const Duration(milliseconds: 1200),
+      () => Get.offAndToNamed("/login"), ///NAVIGATES AND CLEARS SPLASH SCREEN FROM STACK
+    );
+    
     return Scaffold(
+      
+      ///FULLSCREEN IMAGE AS BACKGROUND FOR SPLASHSCREEN
       body: Image.asset(
-        "assets/images/splash.png",height: Get.height,width: Get.width,fit: BoxFit.cover,
+        "assets/images/splash.png",
+        height: Get.height,
+        width: Get.width,
+        fit: BoxFit.cover,  
       ),
     );
-}
+  }
 }
