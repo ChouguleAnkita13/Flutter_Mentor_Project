0c348d500976220a57daab3673be174ff402fe0c
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-19 15:33:36 +0530

    comments added

--- Wallet_App/wallet_app/lib/controller/getpages_controller.dart
+++ Wallet_App/wallet_app/lib/controller/getpages_controller.dart
@@ -13,7 +13,7 @@ import 'package:wallet_app/view/TransferScreen/transfer_screen.dart';
 import 'package:wallet_app/view/aboutus_screen.dart';
 import 'package:wallet_app/view/bottom_navigation_screen.dart';
 import 'package:wallet_app/view/splash_screen.dart';
-
+/// APPLICATION ROUTES AND THEIR ASSOCIATED SCREENS
 class GetpagesController {
   static List<GetPage<dynamic>> getPagesList = [
     GetPage(name: "/", page: () => const SplashScreen()),
