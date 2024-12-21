a62d88357cf51aef171d05f8100ee82e6ca5e735
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-21 11:34:02 +0530

    initialize firebase

--- Grocery_App/grocery_bloc_app/lib/main.dart
+++ Grocery_App/grocery_bloc_app/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_bloc_app/view/HomeScreen/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCy8EyaoL0QqzRvg4_ikC0N9_teTEKJbaQ",
+        appId: "1:230809085504:android:acc1592c128ab7978421a5",
+        messagingSenderId: "230809085504",
+        projectId: "grocery-app-e2c5d"),
+  );
   runApp(const MainApp());
 }
 
