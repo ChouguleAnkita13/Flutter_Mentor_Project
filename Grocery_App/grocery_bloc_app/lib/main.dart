989a2c81875c9f17269eac3e077311b41488c595
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-21 13:00:59 +0530

    commit

--- Grocery_App/grocery_bloc_app/lib/main.dart
+++ Grocery_App/grocery_bloc_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:grocery_bloc_app/view/HomeScreen/home_screen.dart';
+// import 'package:grocery_bloc_app/view/HomeScreen/home_screen.dart';
+import 'package:grocery_bloc_app/view/LoginRegister/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +21,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
