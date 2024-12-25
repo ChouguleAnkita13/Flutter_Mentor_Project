f8e7b1ac723ae054eee36e2c7546a7bd9a8193fc
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-25 20:43:21 +0530

    appbar

--- Grocery_App/grocery_bloc_app/lib/view/HomeScreen/home_screen.dart
+++ Grocery_App/grocery_bloc_app/lib/view/HomeScreen/home_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:grocery_bloc_app/controller/HomeBloc/home_bloc.dart';
 import 'package:grocery_bloc_app/controller/HomeBloc/home_event.dart';
 import 'package:grocery_bloc_app/controller/HomeBloc/home_state.dart';
-import 'package:grocery_bloc_app/controller/SharedPrefernce/session_data.dart';
 import 'package:grocery_bloc_app/view/CartScreen/cart_screen.dart';
+import 'package:grocery_bloc_app/view/HomeScreen/Widget/home_appbar.dart';
 import 'package:grocery_bloc_app/view/HomeScreen/Widget/product_tile_widget.dart';
-import 'package:grocery_bloc_app/view/LoginRegister/Widget/custom_textfield.dart';
 import 'package:grocery_bloc_app/view/WishlistScreen/wishlist_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -58,78 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
           case const (HomeLoadedSuccessState):
             final successState = state as HomeLoadedSuccessState;
             return Scaffold(
-              appBar: AppBar(
-                backgroundColor: const Color.fromRGBO(14, 161, 125, 1),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20))),
-                toolbarHeight: 150,
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        const CircleAvatar(
-                            child: Icon(
-                          Icons.person,
-                          color: Color.fromRGBO(14, 161, 125, 1),
-                        )),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          'Hi, ${SessionData.username}',
-                          style: GoogleFonts.poppins(
-                            textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: CustomTextfield(
-                        title: "Search",
-                        textcontroller: TextEditingController(),
-                      ),
-                    ),
-                  ],
-                ),
-                bottom: PreferredSize(
-                  preferredSize: const Size.fromHeight(60.0),
-                  child: Transform.translate(
-                    offset: const Offset(0, 25),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(
-                        "assets/images/veggie.png",
-                        height: 100,
-                        width: 350,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                actions: [
-                  // IconButton(
-                  //     onPressed: () {
-                  //       homeBloc.add(HomeWishlistButtonNavigateEvent());
-                  //     },
-                  //     icon: const Icon(Icons.favorite_border)),
-                  IconButton(
-                      onPressed: () {
-                        homeBloc.add(HomeCartButtonNavigateEvent());
-                      },
-                      icon: const Icon(Icons.shopping_bag_outlined)),
-                ],
-              ),
+              appBar: HomeAppbar.appBar(homeBloc),
               body: ListView.builder(
                   itemCount: successState.products.length,
                   itemBuilder: (context, index) {
