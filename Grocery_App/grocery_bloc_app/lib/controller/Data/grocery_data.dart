9f92506255d21bafd1ff8c289951f7b86893608b
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-28 15:13:17 +0530

    updated

--- Grocery_App/grocery_bloc_app/lib/controller/Data/grocery_data.dart
+++ Grocery_App/grocery_bloc_app/lib/controller/Data/grocery_data.dart
@@ -1,35 +1,286 @@
 class GroceryData {
   static List<Map<String, dynamic>> groceryProducts = [
     {
-      'id': '1',
-      'name': 'Bananas',
-      'description': 'Fresh bananas from Ecuador',
-      'price': 0.49,
-      'imageUrl':
-          'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+      'category': "Fruits",
+      'itemList': [
+        {
+          'name': 'Bananas',
+          'quantity': "1kg",
+          'description':
+              'Fresh, sweet bananas sourced from top-quality farms. Perfect for smoothies, desserts, and on-the-go snacking. A rich source of potassium and natural energy.',
+          'price': 0.49,
+          'imageUrl':
+              'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+        },
+        {
+          'name': 'Apples',
+          'quantity': "1kg",
+          'description':
+              'Crisp and juicy red apples picked at peak ripeness. Ideal for snacking, baking pies, or adding a refreshing crunch to salads. High in fiber and antioxidants.',
+          'price': 2.99,
+          'imageUrl':
+              'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+        },
+        {
+          'name': 'Oranges',
+          'quantity': "1kg",
+          'description':
+              'Bright and tangy oranges packed with Vitamin C. Perfect for fresh juices, zesting for recipes, or eating as a refreshing snack.',
+          'price': 1.99,
+          'imageUrl':
+              'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+        },
+        {
+          'name': 'Strawberries',
+          'quantity': "500g",
+          'description':
+              'Luscious, red strawberries with a sweet and slightly tart flavor. Great for desserts, breakfast bowls, or dipping in chocolate.',
+          'price': 3.49,
+          'imageUrl':
+              'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+        },
+        {
+          'name': 'Pineapple',
+          'quantity': "1 pc",
+          'description':
+              'Golden tropical pineapple with a juicy, sweet flavor. Perfect for grilling, blending into smoothies, or adding a tropical twist to salads.',
+          'price': 1.99,
+          'imageUrl':
+              'https://cdn.shopify.com/s/files/1/0258/4307/3103/products/asset_2_800x.jpg?v=1571839043',
+        },
+        {
+          'name': 'Grapes',
+          'quantity': "500g",
+          'description':
+              'Seedless, plump green grapes bursting with sweetness. A healthy snack, great for fruit platters or adding to lunch boxes.',
+          'price': 2.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtSge7AGpb8Iq8SLhhreoOX1IfK55nH-n3Hg&s',
+        },
+      ]
     },
     {
-      'id': '2',
-      'name': 'Grapes',
-      'description': 'Organic, locally grown apples',
-      'price': 1.99,
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtSge7AGpb8Iq8SLhhreoOX1IfK55nH-n3Hg&s'
+      'category': "Vegetables",
+      'itemList': [
+        {
+          'name': 'Carrots',
+          'quantity': "1kg",
+          'description':
+              'Bright orange, crunchy carrots that are rich in beta-carotene. Excellent for salads, soups, or snacking raw for a healthy treat.',
+          'price': 1.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgd0I7sZ-qvut2ghl-UhyxKSHkTV7RCR8TA&s',
+        },
+        {
+          'name': 'Tomatoes',
+          'quantity': "1kg",
+          'description':
+              'Fresh, ripe tomatoes with a juicy texture and tangy flavor. Perfect for sauces, salads, or cooking hearty meals.',
+          'price': 0.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXElcirV03FJwU1EONSuYOXl9VyNuHQDzwmw&s',
+        },
+        {
+          'name': 'Spinach',
+          'quantity': "500g",
+          'description':
+              'Leafy green spinach with tender leaves and a slightly earthy flavor. Rich in iron and vitamins, perfect for salads, stir-fries, or soups.',
+          'price': 1.29,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXElcirV03FJwU1EONSuYOXl9VyNuHQDzwmw&s',
+        },
+        {
+          'name': 'Potatoes',
+          'quantity': "1kg",
+          'description':
+              'Versatile and starchy potatoes, ideal for roasting, mashing, or frying. A pantry essential for a variety of recipes.',
+          'price': 0.79,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXElcirV03FJwU1EONSuYOXl9VyNuHQDzwmw&s',
+        },
+        {
+          'name': 'Cucumber',
+          'quantity': "1kg",
+          'description':
+              'Cool and refreshing cucumbers with a crisp bite. Great for hydrating snacks, salads, or sandwiches.',
+          'price': 0.89,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXElcirV03FJwU1EONSuYOXl9VyNuHQDzwmw&s',
+        },
+        {
+          'name': 'Bell Peppers',
+          'quantity': "3 pcs",
+          'description':
+              'A mix of colorful bell peppers with a sweet and crisp flavor. Perfect for stir-fries, grilling, or stuffing with your favorite fillings.',
+          'price': 2.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXElcirV03FJwU1EONSuYOXl9VyNuHQDzwmw&s',
+        },
+      ]
     },
     {
-      'id': '3',
-      'name': 'Bread',
-      'description': 'Brown Bread, Tasty',
-      'price': 2.49,
-      'imageUrl': 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg',
+      'category': "Dairy",
+      'itemList': [
+        {
+          'name': 'Milk',
+          'quantity': "1L",
+          'description':
+              'Rich and creamy fresh milk, sourced from high-quality dairy farms. Perfect for cereals, coffee, or enjoying as is.',
+          'price': 1.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzeRYfXTWA0YF5B6bF2BD5_yVxqQfoJXp4kg&s',
+        },
+        {
+          'name': 'Cheese',
+          'quantity': "200g",
+          'description':
+              'Sharp and flavorful aged cheddar cheese. Great for sandwiches, pastas, or enjoying with crackers.',
+          'price': 2.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzeRYfXTWA0YF5B6bF2BD5_yVxqQfoJXp4kg&s',
+        },
+        {
+          'name': 'Butter',
+          'quantity': "250g",
+          'description':
+              'Creamy salted butter that spreads easily and enhances the flavor of any dish. Ideal for baking or cooking.',
+          'price': 1.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzeRYfXTWA0YF5B6bF2BD5_yVxqQfoJXp4kg&s',
+        },
+        {
+          'name': 'Yogurt',
+          'quantity': "500g",
+          'description':
+              'Tangy and creamy natural yogurt, rich in probiotics. Great for a healthy snack or using as a base for dips.',
+          'price': 1.29,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzeRYfXTWA0YF5B6bF2BD5_yVxqQfoJXp4kg&s',
+        },
+        {
+          'name': 'Eggs',
+          'quantity': "12 pcs",
+          'description':
+              'Fresh, free-range eggs with a rich yolk. Packed with protein, perfect for breakfast or baking.',
+          'price': 2.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuPuWw_9TVjz5Y6ovDrTxg6N2ksUKahS8Kvw&s',
+        },
+        {
+          'name': 'Cream',
+          'quantity': "200ml",
+          'description':
+              'Luxurious heavy cream, perfect for adding richness to desserts, soups, or sauces.',
+          'price': 1.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzeRYfXTWA0YF5B6bF2BD5_yVxqQfoJXp4kg&s',
+        },
+      ]
     },
     {
-      'id': '4',
-      'name': 'Brown Eggs',
-      'description': 'Farm-fresh brown eggs',
-      'price': 3.99,
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuPuWw_9TVjz5Y6ovDrTxg6N2ksUKahS8Kvw&s'
+      'category': "Bakery",
+      'itemList': [
+        {
+          'name': 'White Bread',
+          'quantity': "400g",
+          'description':
+              'Soft and fluffy white bread, perfect for sandwiches, toast, or enjoying with butter and jam.',
+          'price': 1.49,
+          'imageUrl': 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg',
+        },
+        {
+          'name': 'Whole Wheat Bread',
+          'quantity': "400g",
+          'description':
+              'Healthy whole wheat bread with a hearty texture and nutty flavor. Great for sandwiches and breakfast toast.',
+          'price': 1.69,
+          'imageUrl': 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg',
+        },
+        {
+          'name': 'Croissants',
+          'quantity': "6 pcs",
+          'description':
+              'Buttery and flaky croissants, freshly baked and perfect for breakfast or pairing with coffee.',
+          'price': 4.99,
+          'imageUrl': 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg',
+        },
+        {
+          'name': 'Muffins',
+          'quantity': "6 pcs",
+          'description':
+              'Soft and delicious assorted muffins, perfect for tea-time snacking or a quick dessert.',
+          'price': 3.99,
+          'imageUrl': 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg',
+        },
+      ]
+    },
+    {
+      'category': "Snacks",
+      'itemList': [
+        {
+          'name': 'Potato Chips',
+          'quantity': "200g",
+          'description':
+              'Crispy and lightly salted potato chips, perfect for munching during movie nights or as a party snack. Made from farm-fresh potatoes for an authentic taste.',
+          'price': 1.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+        {
+          'name': 'Chocolate Cookies',
+          'quantity': "150g",
+          'description':
+              'Rich and chewy cookies loaded with chocolate chunks. A delightful treat to pair with milk or coffee, or enjoy as a sweet snack.',
+          'price': 2.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScv8pXNVFXbIXTyDj_E_8zhqelqek9o81dMg&s',
+        },
+        {
+          'name': 'Popcorn',
+          'quantity': "300g",
+          'description':
+              'Light and airy popcorn, pre-popped and flavored with a buttery touch. The ideal snack for gatherings or a cozy movie night.',
+          'price': 1.29,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+        {
+          'name': 'Trail Mix',
+          'quantity': "250g",
+          'description':
+              'A healthy mix of nuts, dried fruits, and seeds. Provides a perfect balance of energy and flavor, great for on-the-go snacking.',
+          'price': 3.99,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+        {
+          'name': 'Pretzels',
+          'quantity': "200g",
+          'description':
+              'Crunchy and slightly salted pretzels with a distinct twisted shape. An excellent snack for dipping into cheese or mustard.',
+          'price': 1.79,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+        {
+          'name': 'Granola Bars',
+          'quantity': "6 pcs",
+          'description':
+              'Wholesome and chewy granola bars, packed with oats, nuts, and honey. Perfect for a quick, nutritious snack anytime, anywhere.',
+          'price': 3.49,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+        {
+          'name': 'Nachos',
+          'quantity': "200g",
+          'description':
+              'Crispy and flavorful nacho chips, perfect for dipping into salsa, guacamole, or cheese sauce. A party favorite snack for everyone to enjoy.',
+          'price': 2.29,
+          'imageUrl':
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWqkxb5IKgmEiT_9ebF4YeStJat_hL6f6g5A&s',
+        },
+      ]
     },
   ];
 }
