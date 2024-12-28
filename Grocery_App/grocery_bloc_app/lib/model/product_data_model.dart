c15021c13ec3f9cb6f66744cc97c01704a2606fb
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-28 15:11:14 +0530

    one more model added

--- Grocery_App/grocery_bloc_app/lib/model/product_data_model.dart
+++ Grocery_App/grocery_bloc_app/lib/model/product_data_model.dart
@@ -1,14 +1,27 @@
+class ProductModel {
+  final String category;
+  final List<ProductDataModel> itemList;
+  const ProductModel({
+    required this.category,
+    required this.itemList,
+  });
+}
+
 class ProductDataModel {
   final String id;
+  final String category;
+
   final String name;
   final String description;
   final double price;
   final String imageUrl;
-  ProductDataModel({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.price,
-    required this.imageUrl,
-  });
+  final String quantity;
+  const ProductDataModel(
+      {required this.id,
+      required this.name,
+      required this.category,
+      required this.description,
+      required this.price,
+      required this.imageUrl,
+      required this.quantity});
 }
