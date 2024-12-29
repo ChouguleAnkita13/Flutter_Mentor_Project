b6344975a9f5e0a86cef2eb4bed46be933ffda75
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-29 23:50:16 +0530

    all products screen

---
+++ Grocery_App/grocery_bloc_app/lib/view/ProductsScreen/product_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:grocery_bloc_app/controller/HomeBloc/home_bloc.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_event.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_state.dart';
+import 'package:grocery_bloc_app/model/product_data_model.dart';
+import 'package:grocery_bloc_app/view/ProductsScreen/product_tile.dart';
+import 'package:grocery_bloc_app/view/Widgets/custom_appbar.dart';
+import 'package:grocery_bloc_app/view/detail_product.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({
+    super.key,
+    required this.products,
+    required this.category,
+  });
+  final List<ProductDataModel> products;
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeBloc homeBloc = HomeBloc();
+
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listenWhen: (previous, current) => current is HomeActionState,
+      buildWhen: (previous, current) => current is! HomeActionState,
+      listener: (context, state) {
+        if (state is HomeProductItemWishlistedActionState) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('Item Wishlisted')));
+        } else if (state is HomeNavigateToProductDetailsActionState) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ProductDetailsScreen(product: state.product)));
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: CustomAppbar.customAppbar(category),
+          body: Padding(
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 20),
+            child: Column(
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 0.7,
+                              mainAxisSpacing: 10,
+                              crossAxisSpacing: 10),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            homeBloc.add(HomeProductDetailsClickedEvent(
+                                clickedProduct: products[index]));
+                          },
+                          child: ProductTile(
+                              homeBloc: homeBloc,
+                              productDataModel: products[index]),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ Grocery_App/grocery_bloc_app/lib/view/ProductsScreen/product_tile.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_bloc.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_event.dart';
+import 'package:grocery_bloc_app/model/product_data_model.dart';
+
+class ProductTile extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  final HomeBloc homeBloc;
+  const ProductTile(
+      {super.key, required this.productDataModel, required this.homeBloc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 150,
+            width: 200,
+            alignment: Alignment.topRight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(productDataModel.imageUrl),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Text(productDataModel.name,
+                style: GoogleFonts.poppins(
+                    fontSize: 18, fontWeight: FontWeight.bold)),
+          ),
+          Text("${productDataModel.quantity} ,price",
+              style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[500])),
+          const SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("\$${productDataModel.price}",
+                  style: GoogleFonts.poppins(
+                      fontSize: 18, fontWeight: FontWeight.bold)),
+              GestureDetector(
+                onTap: () {
+                  homeBloc.add(HomeProductWishlistButtonClickedEvent(
+                      clickedProduct: productDataModel));
+                },
+                child: Container(
+                    height: 35,
+                    width: 35,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(14, 161, 125, 1),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 25,
+                    )),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
