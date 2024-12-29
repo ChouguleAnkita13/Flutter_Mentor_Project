3ed7973dd2644e9192ce93db88c13d2930102200
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-29 23:47:59 +0530

    done with explore screen

---
+++ Grocery_App/grocery_bloc_app/lib/view/ExploreScreen/Widget/explore_product_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_bloc_app/model/product_data_model.dart';
+
+class ExploreProductTile extends StatelessWidget {
+  final ProductModel productDataModel;
+  const ExploreProductTile({super.key, required this.productDataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black)),
+      child: Row(
+        children: [
+          Container(
+            height: 150,
+            width: 200,
+            alignment: Alignment.topRight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(productDataModel.itemList[3].imageUrl),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Text(productDataModel.category,
+              style: GoogleFonts.poppins(
+                  fontSize: 21, fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
--- Grocery_App/grocery_bloc_app/lib/view/ExploreScreen/explore_screen.dart
+++ Grocery_App/grocery_bloc_app/lib/view/ExploreScreen/explore_screen.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:grocery_bloc_app/controller/HomeBloc/home_bloc.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_event.dart';
+import 'package:grocery_bloc_app/controller/HomeBloc/home_state.dart';
+import 'package:grocery_bloc_app/view/ExploreScreen/Widget/explore_product_tile.dart';
+import 'package:grocery_bloc_app/view/LoginRegister/Widget/custom_textfield.dart';
+import 'package:grocery_bloc_app/view/ProductsScreen/product_screen.dart';
 import 'package:grocery_bloc_app/view/Widgets/custom_appbar.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
 
   @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  @override
+  void initState() {
+    homeBloc.add(HomeInitialEvent());
+    super.initState();
+  }
+
+  final HomeBloc homeBloc = HomeBloc();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppbar.customAppbar("Find Products"),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listenWhen: (previous, current) => current is HomeActionState,
+      buildWhen: (previous, current) => current is! HomeActionState,
+      listener: (context, state) {
+        if (state is HomeNavigateToExploreProductsActionState) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProductScreen(
+                        products: state.products,
+                        category: state.category,
+                      )));
+        }
+      },
+      builder: (context, state) {
+        switch (state.runtimeType) {
+          case const (HomeLoadingState):
+            return const Scaffold(
+                body: Center(
+              child: CircularProgressIndicator(),
+            ));
+          case const (HomeLoadedSuccessState):
+            final successState = state as HomeLoadedSuccessState;
+            return Scaffold(
+              backgroundColor: Colors.white,
+              appBar: CustomAppbar.customAppbar("Find Products"),
+              body: SingleChildScrollView(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 1.2,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: CustomTextfield(
+                            title: "Search Products",
+                            textcontroller: TextEditingController()),
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: ListView.builder(
+                            itemCount: successState.products.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  homeBloc.add(
+                                      HomeExploreProductButtonClickedEvent(
+                                          clickedProduct: successState
+                                              .products[index].itemList,
+                                          category: successState
+                                              .products[index].category));
+                                },
+                                child: ExploreProductTile(
+                                    productDataModel:
+                                        successState.products[index]),
+                              );
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+
+          case const (HomeErrorState):
+            return const Scaffold(body: Center(child: Text('Error')));
+          default:
+            return const SizedBox();
+        }
+      },
     );
   }
 }
