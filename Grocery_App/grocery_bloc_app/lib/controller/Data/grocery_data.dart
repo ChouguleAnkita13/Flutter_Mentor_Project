885ce50721ed62bdfc9fbf54fa5799d001c4c668
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-07 11:41:11 +0530

    added numberOfItems key value

--- Grocery_App/grocery_bloc_app/lib/controller/Data/grocery_data.dart
+++ Grocery_App/grocery_bloc_app/lib/controller/Data/grocery_data.dart
@@ -6,6 +6,7 @@ class GroceryData {
         {
           'name': 'Bananas',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Fresh, sweet bananas sourced from top-quality farms. Perfect for smoothies, desserts, and on-the-go snacking. A rich source of potassium and natural energy.',
           'price': 0.49,
@@ -15,6 +16,7 @@ class GroceryData {
         {
           'name': 'Apples',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Crisp and juicy red apples picked at peak ripeness. Ideal for snacking, baking pies, or adding a refreshing crunch to salads. High in fiber and antioxidants.',
           'price': 2.99,
@@ -24,6 +26,7 @@ class GroceryData {
         {
           'name': 'Oranges',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Bright and tangy oranges packed with Vitamin C. Perfect for fresh juices, zesting for recipes, or eating as a refreshing snack.',
           'price': 1.99,
@@ -33,6 +36,7 @@ class GroceryData {
         {
           'name': 'Strawberries',
           'quantity': "500g",
+          'numberOfItems': 1,
           'description':
               'Luscious, red strawberries with a sweet and slightly tart flavor. Great for desserts, breakfast bowls, or dipping in chocolate.',
           'price': 3.49,
@@ -42,6 +46,7 @@ class GroceryData {
         {
           'name': 'Pineapple',
           'quantity': "1 pc",
+          'numberOfItems': 1,
           'description':
               'Golden tropical pineapple with a juicy, sweet flavor. Perfect for grilling, blending into smoothies, or adding a tropical twist to salads.',
           'price': 1.99,
@@ -51,6 +56,7 @@ class GroceryData {
         {
           'name': 'Grapes',
           'quantity': "500g",
+          'numberOfItems': 1,
           'description':
               'Seedless, plump green grapes bursting with sweetness. A healthy snack, great for fruit platters or adding to lunch boxes.',
           'price': 2.49,
@@ -65,6 +71,7 @@ class GroceryData {
         {
           'name': 'Carrots',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Bright orange, crunchy carrots that are rich in beta-carotene. Excellent for salads, soups, or snacking raw for a healthy treat.',
           'price': 1.49,
@@ -74,6 +81,7 @@ class GroceryData {
         {
           'name': 'Tomatoes',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Fresh, ripe tomatoes with a juicy texture and tangy flavor. Perfect for sauces, salads, or cooking hearty meals.',
           'price': 0.99,
@@ -83,6 +91,7 @@ class GroceryData {
         {
           'name': 'Spinach',
           'quantity': "500g",
+          'numberOfItems': 1,
           'description':
               'Leafy green spinach with tender leaves and a slightly earthy flavor. Rich in iron and vitamins, perfect for salads, stir-fries, or soups.',
           'price': 1.29,
@@ -92,6 +101,7 @@ class GroceryData {
         {
           'name': 'Potatoes',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Versatile and starchy potatoes, ideal for roasting, mashing, or frying. A pantry essential for a variety of recipes.',
           'price': 0.79,
@@ -101,6 +111,7 @@ class GroceryData {
         {
           'name': 'Cucumber',
           'quantity': "1kg",
+          'numberOfItems': 1,
           'description':
               'Cool and refreshing cucumbers with a crisp bite. Great for hydrating snacks, salads, or sandwiches.',
           'price': 0.89,
@@ -110,6 +121,7 @@ class GroceryData {
         {
           'name': 'Bell Peppers',
           'quantity': "3 pcs",
+          'numberOfItems': 1,
           'description':
               'A mix of colorful bell peppers with a sweet and crisp flavor. Perfect for stir-fries, grilling, or stuffing with your favorite fillings.',
           'price': 2.99,
@@ -124,6 +136,7 @@ class GroceryData {
         {
           'name': 'Milk',
           'quantity': "1L",
+          'numberOfItems': 1,
           'description':
               'Rich and creamy fresh milk, sourced from high-quality dairy farms. Perfect for cereals, coffee, or enjoying as is.',
           'price': 1.49,
@@ -133,6 +146,7 @@ class GroceryData {
         {
           'name': 'Cheese',
           'quantity': "200g",
+          'numberOfItems': 1,
           'description':
               'Sharp and flavorful aged cheddar cheese. Great for sandwiches, pastas, or enjoying with crackers.',
           'price': 2.99,
@@ -142,6 +156,7 @@ class GroceryData {
         {
           'name': 'Butter',
           'quantity': "250g",
+          'numberOfItems': 1,
           'description':
               'Creamy salted butter that spreads easily and enhances the flavor of any dish. Ideal for baking or cooking.',
           'price': 1.99,
@@ -151,6 +166,7 @@ class GroceryData {
         {
           'name': 'Yogurt',
           'quantity': "500g",
+          'numberOfItems': 1,
           'description':
               'Tangy and creamy natural yogurt, rich in probiotics. Great for a healthy snack or using as a base for dips.',
           'price': 1.29,
@@ -160,6 +176,7 @@ class GroceryData {
         {
           'name': 'Eggs',
           'quantity': "12 pcs",
+          'numberOfItems': 1,
           'description':
               'Fresh, free-range eggs with a rich yolk. Packed with protein, perfect for breakfast or baking.',
           'price': 2.49,
@@ -169,6 +186,7 @@ class GroceryData {
         {
           'name': 'Cream',
           'quantity': "200ml",
+          'numberOfItems': 1,
           'description':
               'Luxurious heavy cream, perfect for adding richness to desserts, soups, or sauces.',
           'price': 1.99,
@@ -183,6 +201,7 @@ class GroceryData {
         {
           'name': 'White Bread',
           'quantity': "400g",
+          'numberOfItems': 1,
           'description':
               'Soft and fluffy white bread, perfect for sandwiches, toast, or enjoying with butter and jam.',
           'price': 1.49,
@@ -191,6 +210,7 @@ class GroceryData {
         {
           'name': 'Whole Wheat Bread',
           'quantity': "400g",
+          'numberOfItems': 1,
           'description':
               'Healthy whole wheat bread with a hearty texture and nutty flavor. Great for sandwiches and breakfast toast.',
           'price': 1.69,
@@ -200,6 +220,7 @@ class GroceryData {
         {
           'name': 'Croissants',
           'quantity': "6 pcs",
+          'numberOfItems': 1,
           'description':
               'Buttery and flaky croissants, freshly baked and perfect for breakfast or pairing with coffee.',
           'price': 4.99,
@@ -209,6 +230,7 @@ class GroceryData {
         {
           'name': 'Muffins',
           'quantity': "6 pcs",
+          'numberOfItems': 1,
           'description':
               'Soft and delicious assorted muffins, perfect for tea-time snacking or a quick dessert.',
           'price': 3.99,
@@ -223,6 +245,7 @@ class GroceryData {
         {
           'name': 'Potato Chips',
           'quantity': "200g",
+          'numberOfItems': 1,
           'description':
               'Crispy and lightly salted potato chips, perfect for munching during movie nights or as a party snack. Made from farm-fresh potatoes for an authentic taste.',
           'price': 1.99,
@@ -232,6 +255,7 @@ class GroceryData {
         {
           'name': 'Chocolate Cookies',
           'quantity': "150g",
+          'numberOfItems': 1,
           'description':
               'Rich and chewy cookies loaded with chocolate chunks. A delightful treat to pair with milk or coffee, or enjoy as a sweet snack.',
           'price': 2.49,
@@ -241,6 +265,7 @@ class GroceryData {
         {
           'name': 'Popcorn',
           'quantity': "300g",
+          'numberOfItems': 1,
           'description':
               'Light and airy popcorn, pre-popped and flavored with a buttery touch. The ideal snack for gatherings or a cozy movie night.',
           'price': 1.29,
@@ -253,12 +278,14 @@ class GroceryData {
           'description':
               'A healthy mix of nuts, dried fruits, and seeds. Provides a perfect balance of energy and flavor, great for on-the-go snacking.',
           'price': 3.99,
+          'numberOfItems': 1,
           'imageUrl':
               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAXYsJD1ixlnDi0AX7E-kDRAITFT-j-N5LCA&s'
         },
         {
           'name': 'Pretzels',
           'quantity': "200g",
+          'numberOfItems': 1,
           'description':
               'Crunchy and slightly salted pretzels with a distinct twisted shape. An excellent snack for dipping into cheese or mustard.',
           'price': 1.79,
@@ -268,6 +295,7 @@ class GroceryData {
         {
           'name': 'Granola Bars',
           'quantity': "6 pcs",
+          'numberOfItems': 1,
           'description':
               'Wholesome and chewy granola bars, packed with oats, nuts, and honey. Perfect for a quick, nutritious snack anytime, anywhere.',
           'price': 3.49,
@@ -277,6 +305,7 @@ class GroceryData {
         {
           'name': 'Nachos',
           'quantity': "200g",
+          'numberOfItems': 1,
           'description':
               'Crispy and flavorful nacho chips, perfect for dipping into salsa, guacamole, or cheese sauce. A party favorite snack for everyone to enjoy.',
           'price': 2.29,
