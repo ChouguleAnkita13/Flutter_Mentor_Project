2a5c0f8958be2e47d4d5dbc9b55395642772ea24
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-12 15:56:38 +0530

    Display all orders

---
+++ Grocery_App/grocery_bloc_app/lib/view/OrderScreen/order_tile.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_bloc_app/model/order_details_model.dart';
+import 'package:grocery_bloc_app/view/OrderDetailsScreen/order_details_screen.dart';
+import 'package:grocery_bloc_app/view/Widgets/get_status_container.dart';
+
+class OrderTile extends StatelessWidget {
+  const OrderTile({super.key, required this.order});
+  final OrderDetails order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          )
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Order ID and Status
+            Text(
+              "Order: #${order.id}",
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 8),
+            // Order Date and Total Amount
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Date: ${order.dateTime}",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                Text(
+                  "\$${order.totalAmount.toStringAsFixed(2)}",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            // Items Preview
+            Text(
+              "Items: ${order.items.map((h) {
+                return h.name;
+              }).toList()}",
+              style: GoogleFonts.poppins(
+                fontSize: 14,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(height: 12),
+            // View Details Button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GetStatusContainer.getStatusContainer(order.status),
+                GestureDetector(
+                  onTap: () {
+                    /// Navigate to Order Details Page
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              OrderDetailsPage(orderDetails: order),
+                        ));
+                  },
+                  child: Text(
+                    "View Details",
+                    style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: const Color.fromRGBO(14, 161, 125, 1)),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Grocery_App/grocery_bloc_app/lib/view/OrderScreen/orderscreen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery_bloc_app/controller/OrderBloc/order_bloc.dart';
+import 'package:grocery_bloc_app/controller/OrderBloc/order_event.dart';
+import 'package:grocery_bloc_app/controller/OrderBloc/order_state.dart';
+import 'package:grocery_bloc_app/view/OrderScreen/order_tile.dart';
+import 'package:grocery_bloc_app/view/Widgets/custom_appbar.dart';
+
+class MyOrdersPage extends StatelessWidget {
+  const MyOrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => OrderBloc()..add(OrderInitialEvent()),
+      child: BlocBuilder<OrderBloc, OrderState>(
+        builder: (context, state) {
+          if (state is OrdersLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is OrdersLoadedSuccessState) {
+            return Scaffold(
+              appBar: CustomAppbar.customAppbar("My Orders"),
+              backgroundColor: Colors.white,
+              body: state.ordersList.isEmpty
+                  ? const Center(child: Text("No Orders Yet"))
+                  : ListView.builder(
+                      padding: const EdgeInsets.all(16.0),
+                      itemCount: state.ordersList.length,
+                      itemBuilder: (context, index) {
+                        final order = state.ordersList[index];
+                        return OrderTile(order: order);
+                      },
+                    ),
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+}
