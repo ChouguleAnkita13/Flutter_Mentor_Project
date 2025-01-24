154b576cb567d7df6341675eaa6233337d640663
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-24 13:55:38 +0530

    remove event added

--- Grocery_App/grocery_bloc_app/lib/controller/NotificationBloc/notification_event.dart
+++ Grocery_App/grocery_bloc_app/lib/controller/NotificationBloc/notification_event.dart
@@ -1,3 +1,11 @@
+import 'package:grocery_bloc_app/model/notification_model.dart';
+
 abstract class NotificationEvent {}
 
 class NotificationInitialEvent extends NotificationEvent {}
+
+class NotificationRemoveEvent extends NotificationEvent {
+  final NotificationModel notification;
+
+  NotificationRemoveEvent({required this.notification});
+}
