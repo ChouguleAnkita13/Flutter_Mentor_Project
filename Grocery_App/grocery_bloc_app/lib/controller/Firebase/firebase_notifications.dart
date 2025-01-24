18fe5a5806e99c773998e909a792ca987e3feac0
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-24 13:57:06 +0530

    remove notification from firebase

--- Grocery_App/grocery_bloc_app/lib/controller/Firebase/firebase_notifications.dart
+++ Grocery_App/grocery_bloc_app/lib/controller/Firebase/firebase_notifications.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:grocery_bloc_app/controller/Firebase/firebase_order_data.dart';
 import 'package:grocery_bloc_app/controller/SharedPrefernce/session_data.dart';
 import 'package:grocery_bloc_app/model/notification_model.dart';
-import 'dart:developer';
 
 class FirebaseNotifications {
   static List<NotificationModel> notificationList = [];
-  static FirebaseFirestore firebaseInstance = FirebaseFirestore.instance;
 
+  ///FIREBASE DOCUMENT INSTANCE
+  static DocumentReference<Map<String, dynamic>> docInstance =
+      FirebaseFirestore.instance.collection("Users").doc(SessionData.email);
+
+  ///FETCH ALL NOTIFICATION FROM FIREBASE
   static Future<void> getNotificationListFromFirebase() async {
-    final docInstance =
-        firebaseInstance.collection("Users").doc(SessionData.email);
     DocumentSnapshot userDoc = await docInstance.get();
 
     notificationList = List.generate(
@@ -17,9 +19,27 @@ class FirebaseNotifications {
         (i) => NotificationModel(
             orderId: userDoc['notificationList'][i]['orderId'],
             message: userDoc['notificationList'][i]['message'],
-            dateTime: userDoc['notificationList'][i]['dateTime']));
-    log("${userDoc['notificationList']}");
+            date: userDoc['notificationList'][i]['date'],
+            time: userDoc['notificationList'][i]['time'])).reversed.toList();
+    // log("${userDoc['notificationList']}");
+  }
 
-    log("${notificationList.runtimeType}");
+  ///Remove Notification from screen
+  static Future<void> removeNotificationFromFirebase(
+      NotificationModel notification) async {
+    ///GET LIST OF ORDERID AND NOTIFICTAIONS
+    await FirebaseOrderData.getOrderIdListFromFirebase();
+
+    ///
+    FirebaseOrderData.notificationListOfMap
+        .removeWhere((note) => note['time'] == notification.time);
+
+    ///REMOVE NOTIFICATION FROM LOCAL LIST
+    notificationList.remove(notification);
+
+    ///ALSO UPDATE FIREBASE NOTIFICATIONLIST FROM USER COLLECTION
+
+    await docInstance
+        .update({'notificationList': FirebaseOrderData.notificationListOfMap});
   }
 }
--- Grocery_App/grocery_bloc_app/lib/controller/NotificationBloc/notification_bloc.dart
+++ Grocery_App/grocery_bloc_app/lib/controller/NotificationBloc/notification_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:grocery_bloc_app/controller/Firebase/firebase_notifications.dart';
 import 'package:grocery_bloc_app/controller/NotificationBloc/notification_event.dart';
@@ -8,12 +7,22 @@ import 'package:grocery_bloc_app/controller/NotificationBloc/notification_state.
 class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
   NotificationBloc() : super(NotificationInitialState()) {
     on<NotificationInitialEvent>(notificationInitialEvent);
+    on<NotificationRemoveEvent>(notificationRemoveEvent);
   }
 
   FutureOr<void> notificationInitialEvent(
       NotificationInitialEvent event, Emitter<NotificationState> emit) async {
     emit(NotificationLoadingState());
     await FirebaseNotifications.getNotificationListFromFirebase();
+    emit(NotificationLoadedSuccessState(
+        notificationList: FirebaseNotifications.notificationList));
+  }
+
+  FutureOr<void> notificationRemoveEvent(
+      NotificationRemoveEvent event, Emitter<NotificationState> emit) async {
+    await FirebaseNotifications.removeNotificationFromFirebase(
+        event.notification);
+
     emit(NotificationLoadedSuccessState(
         notificationList: FirebaseNotifications.notificationList));
   }
