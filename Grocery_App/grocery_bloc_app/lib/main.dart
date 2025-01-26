e1f5acb289728787b5cce1d4d36075b65db249f6
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-26 19:43:50 +0530

    commit

--- Grocery_App/grocery_bloc_app/lib/main.dart
+++ Grocery_App/grocery_bloc_app/lib/main.dart
@@ -2,7 +2,6 @@
 // import 'package:grocery_bloc_app/controller/Data/grocery_data.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:grocery_bloc_app/view/splash_screen.dart';
 
 void main() async {
